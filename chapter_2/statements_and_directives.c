f6bbe572de570b81d7d24812cb7b25791ace7ceb
VesperVanity <OCEAN.jung@protonmail.com>
2025-08-10 19:03:09 +0200

    exercise 2.

---
+++ chapter_2/statements_and_directives.c
@@ -0,0 +1,22 @@
+/*
+*C Programming: A Modern Approach - K.N.King
+*Chapter 2 - C Fundamentals - Exercise 2.
+*File created: 10/08/2025
+*File last modified: 10/08/2025
+*Name: statements_and_directives.c
+*Purpose: a) Identify the directives and statements in this program
+		  b) What output does the program produce?
+*Author: VesperVanity (VV) https://github.com/VesperVanity
+*/
+//The #include is a directive
+#include <stdio.h>
+
+int main(void)
+{
+	//All four are statements, three printf statements, and one return statement
+	//Output is on three lines
+	printf("Parkinson's Law:\nWork expands so as to ");
+	printf("fill the time\n");
+	printf("available for its completion.\n");
+	return 0;
+}
