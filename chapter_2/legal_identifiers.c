4e26f9e460175aac55b2632f31352b6c6832ee19
VesperVanity <OCEAN.jung@protonmail.com>
2025-08-10 19:31:29 +0200

    Exercise 5.

---
+++ chapter_2/legal_identifiers.c
@@ -0,0 +1,28 @@
+/*
+*C Programming: A Modern Approach - K.N.King
+*Chapter 2 - C Fundamentals - Exercise 5.
+*File created: 10/08/2025
+*File last modified: 10/08/2025
+*Name: legal_identifiers.c
+*Purpose: Which of the following are not legal C indentifiers?
+		  (a) 100_bottles
+		  (b) _100_bottles
+		  (c) one_hundred_bottles
+		  (d) bottles_by_the_hundred_
+*Author: VesperVanity (VV) https://github.com/VesperVanity
+*/
+#include <stdio.h>
+
+int main(void)
+{
+	//Illegal
+	int 100_bottles;
+	//Legal
+	int _100_bottles;
+	//Legal
+	int one_hundred_bottles;
+	//Legal
+	int bottles_by_the_hundred_;
+
+	return 0;
+}
