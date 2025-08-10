7718612ce99b38c3b28fee46dcecc76967ea3f97
VesperVanity <OCEAN.jung@protonmail.com>
2025-08-10 18:53:11 +0200

    Init and exercise 1.

---
+++ chapter_2/hello_world.c
@@ -0,0 +1,16 @@
+/*
+*C Programming: A Modern Approach - K.N.King
+*Chapter 2 - C Fundamentals - Exercise 1.
+*File created: 10/08/2025
+*File last modified: 10/08/2025
+*Name: hello_world.c
+*Purpose: Create and run Kernighan and Ritchie's famous "hello, world" program
+*Author: VesperVanity (VV) https://github.com/VesperVanity
+*/
+#include <stdio.h>
+
+int main(void)
+{
+	printf("hello, world\n");
+	return 0;
+}
