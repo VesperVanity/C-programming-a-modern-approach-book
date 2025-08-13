d16262f14380f829c3c0bc393061ddd99580f4e5
VesperVanity <OCEAN.jung@protonmail.com>
2025-08-13 18:40:52 +0200

    Exercise 4.

---
+++ chapter_4/C99_expressions.c
@@ -0,0 +1,34 @@
+/*
+*C Programming: A Modern Approach - K.N.King
+*Chapter 4 - Expressions - Exercise 4.
+*File created: 13/08/2025
+*File last modified: 13/08/2025
+*Name: C99_expressions.c
+*Purpose: Repeat exercise 3. for C99
+*Author: VesperVanity (VV) https://github.com/VesperVanity
+*/
+#include <stdio.h>
+
+int main(void)
+{
+	/*
+		(a) is 1
+		(b) is -1
+		(c) is -1
+		(d) is 1
+	*/
+
+	//(a) is still 1
+	//(b) is still -1
+	//(c) is still -1
+	//(d) is still 1
+
+	
+
+	int exp_1 = -8;
+	int exp_2 = -5;
+
+	printf("%d\n", exp_1 / exp_2);
+
+	return 0;
+}
