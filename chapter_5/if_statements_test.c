4cdf9fff9741567fcef0fe365c87ad8f268e09e6
VesperVanity <OCEAN.jung@protonmail.com>
2025-08-15 20:02:40 +0200

    Chapter 5 init

---
+++ chapter_5/if_statements_test.c
@@ -0,0 +1,16 @@
+/*
+*C Programming: A Modern Approach - K.N.King
+*Chapter 5 - Selection Statements - Exercise 1.
+*File created: 15/08/2025
+*File last modified: 15/08/2025
+*Name: if_statement_test.c
+*Purpose: 
+*Author: VesperVanity (VV) https://github.com/VesperVanity
+*/
+#include <stdio.h>
+
+int main(void)
+{
+
+	return 0;
+}
