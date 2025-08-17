8af4c8e454fee5a916d123331b0a3d79a5813d6b
VesperVanity <OCEAN.jung@protonmail.com>
2025-08-17 18:39:37 +0200

    update state

--- chapter_5/if_statements_test.c
+++ chapter_5/if_statements_test.c
@@ -2,7 +2,7 @@
 *C Programming: A Modern Approach - K.N.King
 *Chapter 5 - Selection Statements - Exercise 1.
 *File created: 15/08/2025
-*File last modified: 16/08/2025
+*File last modified: 17/08/2025
 *Name: if_statement_test.c
 *Purpose: 
 *Author: VesperVanity (VV) https://github.com/VesperVanity
@@ -11,6 +11,6 @@
 
 int main(void)
 {
-	
+	printf("%s\n", "Still taking notes from the chapter, lots and lots of writing pages.");
 	return 0;
 }
