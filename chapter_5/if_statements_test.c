0746e20da886288191ec101d1238bd1de59618a4
VesperVanity <OCEAN.jung@protonmail.com>
2025-08-18 15:17:08 +0200

    Exercise 1.

--- chapter_5/if_statements_test.c
+++ chapter_5/if_statements_test.c
@@ -2,15 +2,38 @@
 *C Programming: A Modern Approach - K.N.King
 *Chapter 5 - Selection Statements - Exercise 1.
 *File created: 15/08/2025
-*File last modified: 17/08/2025
+*File last modified: 18/08/2025
 *Name: if_statement_test.c
-*Purpose: 
+*Purpose: The following program fragments illustrate the relational and
+		  equality operators
+		  Show the output produced by each, assuming that
+		  i, j and k are int variables
+		  (a) i = 2; j = 3;
+		  	  k = i * j == 6;
+		  	  printf("%d", k);
+		  (b) i = 5; j = 10; k = 1;
+		  	  printf("%d", k > i < j);
+		  (c) i = 3; j = 2; k = 1;
+		  	  printf("%d", i < j == j < k);
+		  (d) i = 3; j = 4; k = 5;
+		  	  printf("%d", i % j + i < k);
 *Author: VesperVanity (VV) https://github.com/VesperVanity
 */
 #include <stdio.h>
 
 int main(void)
 {
-	printf("%s\n", "Still taking notes from the chapter, lots and lots of writing pages.");
+	/*
+		(a) output is 1
+		(b) output is 1
+		(c) output is 1
+		(d) output is 0
+	*/
+
+	int i = 0, j = 0, k = 0;
+
+	i = 3; j = 4; k = 5;
+	printf("%d", i % j + i < k);
+
 	return 0;
 }
