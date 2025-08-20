911a4b215609268c0c9ae307818cfa936e9265ff
VesperVanity <OCEAN.jung@protonmail.com>
2025-08-20 14:24:30 +0200

    Re-init

---
+++ chapter_5/broker_update.c
@@ -0,0 +1,24 @@
+/*
+*C Programming: A Modern Approach - K.N.King
+*Chapter 5 - Selection Statements - Project 3.
+*File created: 20/08/2025
+*File last modified: 20/08/2025
+*Name: broker_update.c
+*Purpose: Modify the broker.c program of section 5.2 by making both of the following
+		  changes:
+		  (a) Ask the user to enter the number of shares and the price per share,
+		  	  instead of the value of the trade
+		  (b) Add statements that compute the commission charged by a rival broker
+		  	  ($33 plus 3 cent per share for fewer than 2000 shares, $33 plus 2 cent
+		  	  per share for 2000 shares or more)
+		  	  Display the rival's commission as well as the commission charged
+		  	  by the original broker
+*Author: VesperVanity (VV) https://github.com/VesperVanity
+*/
+#include <stdio.h>
+
+int main(void)
+{
+	
+	return 0;
+}
