643f1249c2dea4a07515b9b25491dec4acf7d6e5
VesperVanity <OCEAN.jung@protonmail.com>
2025-08-27 19:27:15 +0200

    Project 3.

--- chapter_5/broker_update.c
+++ chapter_5/broker_update.c
@@ -33,28 +33,25 @@
 
 int main(void)
 {
-  float commission, value;
+  float commission = 0.0f, rival_commission = 0.0f;
 
-  printf("Enter value of trade: ");
-  scanf("%f", &value);
+  printf("%s", "Enter the number of shares: ");
+  int number_of_shares = 0;
+  scanf("%d", &number_of_shares);
 
-  if (value < 2500.00f)
-    commission = 30.00f + .017f * value;
-  else if (value < 6250.00f)
-    commission = 56.00f + .0066f * value;
-  else if (value < 20000.00f)
-    commission = 76.00f + .0034f * value;
-  else if (value < 50000.00f)
-    commission = 100.00f + .0022f * value;
-  else if (value < 500000.00f)
-    commission = 155.00f + .0011f * value;
-  else
-    commission = 255.00f + .0009f * value;
+  printf("%s", "Enter the price per share: ");
+  float price_per_share = 0.0f;
+  scanf("%f", &price_per_share);
 
-  if (commission < 39.00f)
-    commission = 39.00f;
+  if (number_of_shares < 2000)
+    rival_commission = price_per_share + 0.03f * number_of_shares;
+  else if (number_of_shares >= 2000)
+    rival_commission = price_per_share + 0.02f * number_of_shares;
+  
+  commission = price_per_share + 0.00f * number_of_shares;
 
   printf("Commission: $%.2f\n", commission);
+  printf("Rival's Commission: $%.2f\n", rival_commission);
 
   return 0;
 }
