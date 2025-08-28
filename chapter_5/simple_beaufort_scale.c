e63757f85bc550ccc29c6fb1bf484c476df8b596
VesperVanity <OCEAN.jung@protonmail.com>
2025-08-28 18:39:39 +0200

    Project 4.

--- chapter_5/simple_beaufort_scale.c
+++ chapter_5/simple_beaufort_scale.c
@@ -21,6 +21,24 @@
 
 int main(void)
 {
-	
+	printf("%s", "Enter wind speed in knots: ");
+	int wind_speed_input = 0;
+	scanf("%d", &wind_speed_input);
+
+	printf("%s\t%s\n", "Speed(knots)", "Description");
+
+	if(wind_speed_input < 1)
+		printf("%s\t%s\n", "Less than 1", "Calm");
+	else if(wind_speed_input >= 1 && wind_speed_input <= 3)
+		printf("%s\t%s\n", "1-3", "Light air");
+	else if(wind_speed_input >= 4 && wind_speed_input <= 27)
+		printf("%s\t%s\n", "4-27", "Breeze");
+	else if(wind_speed_input >= 28 && wind_speed_input <= 47)
+		printf("%s\t%s\n", "28-47", "Gale");
+	else if(wind_speed_input >= 48 && wind_speed_input <= 63)
+		printf("%s\t%s\n", "48-63", "Storm");
+	else if(wind_speed_input > 63)
+		printf("%s\t%s\n", "Above 63", "Hurricane");
+
 	return 0;
 }
